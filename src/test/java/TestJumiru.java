0d36fb6132c2edfd03246a35b169c61ed1d7dfcf
Michal Urban <485429@mail.muni.cz>
2022-03-09 12:54:55 +0100

    fixed the failing test

--- src/test/java/TestJumiru.java
+++ src/test/java/TestJumiru.java
@@ -8,6 +8,5 @@ public class TestJumiru {
     public void testValid(){
         assertTrue(true);
         assertEquals("A1LQFN3A", TestVector.VALID_BECH32M[0]);
-        assertEquals("A1LQFN3Q", TestVector.VALID_BECH32M[0]);
     }
 }
