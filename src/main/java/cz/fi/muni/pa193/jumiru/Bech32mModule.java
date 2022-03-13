8fd15742fc92f023b7661883e2f11cb566ae1e71
Michal Urban <485429@mail.muni.cz>
2022-03-13 08:12:26 +0100

    simple refactoring

--- src/main/java/cz/fi/muni/pa193/jumiru/Bech32mModule.java
+++ src/main/java/cz/fi/muni/pa193/jumiru/Bech32mModule.java
@@ -13,36 +13,23 @@ public final class Bech32mModule implements Bech32mTransformer {
             -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
             15, -1, 10, 17, 21, 20, 26, 30,  7,  5, -1, -1, -1, -1, -1, -1,
             -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
-            1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1,
+             1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1,
             -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
-            1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1
+             1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1
     };
 
     private int bech32mPolymod(List<Byte> expandedParts) {
         int c = 1;
+        int[] GEN = {0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3};
         for (byte value: expandedParts) {
             byte c0 = (byte) (c >>> 25);
-
             c = ((c & 0x1ffffff) << 5) ^ value;
-
-            if ((c0 & 1) == 1) {
-                c ^= 0x3b6a57b2;
-            }
-
-            if ((c0 & 2) == 2) {
-                c ^= 0x26508e6d;
-            }
-
-            if ((c0 & 4) == 4) {
-                c ^= 0x1ea119fa;
-            }
-
-            if ((c0 & 8) == 8) {
-                c ^= 0x3d4233dd;
-            }
-
-            if ((c0 & 16) == 16) {
-                c ^= 0x2a1462b3;
+            int p = 1;
+            for (int i = 0; i<5; i++) {
+                if ((c0 & p) == p) {
+                    c ^= GEN[i];
+                }
+                p *= 2;
             }
         }
         return c;
@@ -79,22 +66,14 @@ public final class Bech32mModule implements Bech32mTransformer {
     }
 
     public boolean checkBech32mString(String str) {
-
-        if (str == null) {
-            return false;
-        }
-
-        if (!str.equals(str.toLowerCase()) &&
-                !str.equals(str.toUpperCase())) {
-            return false;
-        }
+        if (str == null) return false;
+        if (!str.equals(str.toLowerCase()) && !str.equals(str.toUpperCase())) return false;
 
         str = str.toLowerCase();
 
         boolean dataPart = true;
         int separatorPos = 0;
         for (int i = str.length() - 1; i >= 0; i--) {
-
             if (str.charAt(i) == '1' && dataPart) {
                 dataPart = false;
                 separatorPos = i;
@@ -102,63 +81,46 @@ public final class Bech32mModule implements Bech32mTransformer {
             }
 
             if (dataPart) {
-                if (CHARSET.indexOf(str.charAt(i)) == -1) {
-                    return false;
-                }
+                if (CHARSET.indexOf(str.charAt(i)) == -1) return false;
             } else {
-                if (str.charAt(i) < 33 || str.charAt(i) > 126) {
-                    return false;
-                }
+                if (str.charAt(i) < 33 || str.charAt(i) > 126) return false;
             }
         }
 
-        if (dataPart) {
-            return false;
-        }
-
-        if (separatorPos == 0 || separatorPos >= 84) {
-            return false;
-        }
-
-        if (str.length() - separatorPos < 7) {
-            return false;
-        }
-
+        if (dataPart) return false;
+        if (separatorPos == 0 || separatorPos >= 84) return false;
+        if (str.length() - separatorPos < 7) return false;
         return true;
     }
 
     public Bech32mIOData decodeBech32mString(String str) {
-
-        if (!checkBech32mString(str)) {
-            throw new IllegalArgumentException();
-        }
+        if (!checkBech32mString(str)) throw new IllegalArgumentException();
 
         int separatorPos = str.lastIndexOf('1');
 
         String hrPart = str.substring(0, separatorPos).toLowerCase();
         List<Byte> data = decodeDataPart(str.substring(separatorPos + 1));
 
-        if (!verifyChecksum(hrPart, data)) {
-            throw new IllegalArgumentException();
-        }
+        if (!verifyChecksum(hrPart, data)) throw new IllegalArgumentException();
 
         return new Bech32mIOData(hrPart, data.subList(0, data.size() - 6));
     }
 
     @Override
     public String encodeBech32mString(Bech32mIOData input) {
-        String output = input.getHrPart();
-        if (!output.equals(output.toLowerCase())) {
-            throw new IllegalArgumentException();
-        }
+        String hrPart = input.getHrPart();
+        if (!hrPart.equals(hrPart.toLowerCase())) throw new IllegalArgumentException();
+
+        StringBuilder output = new StringBuilder(hrPart);
+        output.append('1');
+
         List<Byte> data = new ArrayList<>(input.getDataPart());
         data.addAll(calculateChecksum(input));
-        output = output.concat("1");
         for (byte c: data) {
-            //convert to StringBuilder
-            output = output + CHARSET.charAt(c);
+            output.append(CHARSET.charAt(c));
         }
-        return output;
+
+        return output.toString();
     }
 
     public List<Byte> calculateChecksum(Bech32mIOData input) {
