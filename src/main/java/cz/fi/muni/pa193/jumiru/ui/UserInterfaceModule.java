7e69b59f801c78b696e1d420dcde3ea365a492d9
Michal Urban <485429@mail.muni.cz>
2022-04-03 02:23:51 +0200

    added the dataPart reading functionality

--- src/main/java/cz/fi/muni/pa193/jumiru/ui/UserInterfaceModule.java
+++ src/main/java/cz/fi/muni/pa193/jumiru/ui/UserInterfaceModule.java
@@ -3,6 +3,12 @@ package cz.fi.muni.pa193.jumiru.ui;
 import cz.fi.muni.pa193.jumiru.Bech32mException;
 import cz.fi.muni.pa193.jumiru.UserInterfaceException;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class UserInterfaceModule implements UserInterface{
     private boolean encode;
     private int argIndex = 0;
@@ -15,6 +21,7 @@ public class UserInterfaceModule implements UserInterface{
     private String inputFileName;
     private String outputFileName;
     private String HRP;
+    private String inputDataPart;
 
 
 
@@ -64,6 +71,7 @@ public class UserInterfaceModule implements UserInterface{
     }
 
     private void parseFilename(boolean input) {
+        argIndex++;
         if (input){
             hasNextArg("FileName argument is missing");
         }
@@ -72,36 +80,42 @@ public class UserInterfaceModule implements UserInterface{
         } else {
             outputFileName = args[argIndex];
         }
+    }
+
+    private void parseDataPart() {
         argIndex++;
+        hasNextArg("Data part argument is missing");
+        inputDataPart = args[argIndex];
     }
 
     private void parseIODestination(boolean input) {
         hasNextArg("IO destination argument (stdin/file/arg/stdout) is missing");
         switch (args[argIndex]) {
             case "arg":
-                if (input)
-                    inputDestination = IODestinationEnum.arg;
-                else
-                    outputDestination = IODestinationEnum.arg;
+                if (!input)
+                    throw new UserInterfaceException("Error in argument " + argIndex + "(" +
+                            args[argIndex] + "): arg cannot be selected as output destination");
+                inputDestination = IODestinationEnum.arg;
+                parseDataPart();
                 break;
             case "file":
                 if (input)
-                    inputDestination = IODestinationEnum.arg;
+                    inputDestination = IODestinationEnum.file;
                 else
-                    outputDestination = IODestinationEnum.arg;
+                    outputDestination = IODestinationEnum.file;
                 parseFilename(input);
                 break;
             case "stdin":
                 if (!input)
                     throw new UserInterfaceException("Error in argument " + argIndex + "(" +
-                            args[argIndex] + "): stdin cannot be selected for output destination");
-                outputDestination = IODestinationEnum.stdin;
+                            args[argIndex] + "): stdin cannot be selected as output destination");
+                inputDestination = IODestinationEnum.stdin;
                 break;
             case "stdout":
                 if (input)
                     throw new UserInterfaceException("Error in argument " + argIndex + "(" +
-                            args[argIndex] + "): stdout cannot be selected for input destination");
-                inputDestination = IODestinationEnum.stdout;
+                            args[argIndex] + "): stdout cannot be selected as input destination");
+                outputDestination = IODestinationEnum.stdout;
                 break;
             default:
                 throw new UserInterfaceException("Argument " + argIndex + "(" + args[argIndex] +
@@ -112,6 +126,35 @@ public class UserInterfaceModule implements UserInterface{
 
     private void parseHRP() {
         hasNextArg("The HRP argument is missing");
+        HRP = args[argIndex];
+        argIndex++;
+    }
+
+    private void loadInputData() {
+        if (inputDestination == IODestinationEnum.stdin) {
+            System.out.println("Enter the data part to be encoded:");
+            Scanner scanner = new Scanner(System.in);
+            inputDataPart = scanner.nextLine();
+            scanner.close();
+        }
+        if (inputDestination == IODestinationEnum.file) {
+            File inputFile =  new File(inputFileName);
+            if (!inputFile.isFile())
+                throw new UserInterfaceException("The provided input file does not exist");
+            try {
+                FileInputStream fileInputStream = new FileInputStream(inputFile);
+                byte[] chars = new byte[(int) inputFile.length()];
+                int readBytes = fileInputStream.read(chars);
+                if (readBytes != (int) inputFile.length())
+                    throw new UserInterfaceException("Some data from file could not be read " +
+                            "successfully");
+                fileInputStream.close();
+                inputDataPart = new String(chars);
+            }
+            catch (IOException e) {
+                throw new UserInterfaceException("The read from file failed due to the following exception: " + e.getMessage());
+            }
+        }
     }
 
     private void entryPoint() {
@@ -121,10 +164,12 @@ public class UserInterfaceModule implements UserInterface{
         parseIODestination(false);
         if (encode)
             parseHRP();
-        if (args.length > argIndex + 1) {
+        if (args.length > argIndex) {
             throw new UserInterfaceException("Following the argument: " + args[argIndex] +
                     ", there is one or more unnecessary arguments.");
         }
+
+        loadInputData();
     }
 
     public void entryPointWrapper() {
