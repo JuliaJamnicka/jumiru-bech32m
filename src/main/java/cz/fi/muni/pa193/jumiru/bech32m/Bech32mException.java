ae00afd5cd94e9c51b24997e81be3e96cd4b7436
JuliaJamnicka <julia.jamnicka@gmail.com>
2022-04-18 23:51:50 +0200

    Create review branch

--- src/main/java/cz/fi/muni/pa193/jumiru/bech32m/Bech32mException.java
+++
@@ -1,7 +0,0 @@
-package cz.fi.muni.pa193.jumiru.bech32m;
-
-public class Bech32mException extends RuntimeException {
-    public Bech32mException(String err) {
-        super(err);
-    }
-}
--- src/main/java/cz/fi/muni/pa193/jumiru/ui/UserInterfaceException.java
+++
@@ -1,7 +0,0 @@
-package cz.fi.muni.pa193.jumiru.ui;
-
-class UserInterfaceException extends RuntimeException {
-    UserInterfaceException(final String err) {
-        super(err);
-    }
-}
